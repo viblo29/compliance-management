521f132550bf44373ca983ada54bb360e93e4f30
viblo29 <dachivibliani29@gmail.com>
2026-04-03 02:49:10 +0400

    feat/FAQ-page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,3 +1,4 @@
+import FAQ from '@/components/organisms/FAQ'
 import Home from '@/components/organisms/Home'
 import Home2 from '@/components/organisms/Home2'
 import Home3 from '@/components/organisms/Home3'
@@ -9,6 +10,7 @@ function page() {
     <Home/>
     <Home2/>
     <Home3/>
+    <FAQ/>
     </>
   )
 }
